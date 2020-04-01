67ac977185cd23424115247b5ec4328b08e0764d
antbz <antonio.bezerra@fe.up.pt>
2020-04-01 15:50:43 +0100

    Added fourth Documents

--- test_query.sql
+++ test_query.sql
@@ -1,5 +1,4 @@
 -- SQLite
-SELECT Belongs.WID, CRname, COUNT(docID)
-FROM Belongs, ProjectRole 
-WHERE Belongs.WID = ProjectRole.WID
-GROUP BY ProjectRole.WID, Belongs.CRname;
+SELECT Belongs.WID, CRname
+FROM Belongs, CompanyRole 
+WHERE Belongs.CRID = CompanyRole.CRID;
