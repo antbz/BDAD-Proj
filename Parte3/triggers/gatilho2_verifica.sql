a5e31502f583f3194d7f378419a0261696bedcfc
Ca-moes <andre3000pt@gmail.com>
2020-05-19 21:04:56 +0100

    Trigger2

--- Parte3/triggers/gatilho2_verifica.sql
+++ Parte3/triggers/gatilho2_verifica.sql
@@ -1,1 +1,15 @@
 -- Ao alterar Status ou Remarks de Type -> atualizar last_mod_date
+
+.mode columns
+.headers on
+.nullvalue NULL
+
+SELECT * FROM Type;
+
+UPDATE Type SET Remarks = 'mudou' where TID = 1;
+
+SELECT * FROM Type;
+
+UPDATE Type SET Status = 0 where TID = 2;
+
+SELECT * FROM Type;
--- test_query.sql
+++ test_query.sql
@@ -1,2 +1,12 @@
 -- SQLite
-DELETE FROM Type WHERE TID=1;
+
+-- Ao alterar Status ou Remarks de Type -> atualizar last_mod_date
+PRAGMA foreign_keys = ON;
+
+CREATE TRIGGER gatilho2
+AFTER UPDATE OF Status, Remarks ON Type 
+BEGIN
+    Update Type
+    SET last_mod_date =  DATE('now')
+    WHERE TID = old.TID;
+END;
