c2c98ef73f26ae394ae4cc3a5298d0a4d10bbcc3
filipeeteixeira <filipeteixeira00@hotmail.com>
2020-05-19 20:49:15 +0100

    Added trigger3

--- Parte3/triggers/gatilho3_adiciona.sql
+++ Parte3/triggers/gatilho3_adiciona.sql
@@ -1,1 +1,23 @@
--- Só deixar um worker criar uma nova revisão se tiver feito da última revisão anteriormente
+-- Só deixar um worker criar uma nova revisão se tiver feito CheckOut da última revisão anteriormente
+
+PRAGMA foreign_keys = ON;
+
+CREATE TRIGGER gatilho3
+BEFORE INSERT ON Revision
+WHEN (
+	(
+	SELECT max(revID) --id da ultima revisao do documento
+	FROM Revision 
+	WHERE docID=New.docID
+	)
+	IS NOT
+	(
+	SELECT max(revID) --id do ultimo CheckOut que o autor da nova revisao efetuou para aquele documento
+	FROM CheckOut 
+	JOIN Revision using(revID) 
+	WHERE WID=New.authorID AND docID=New.docID
+	)
+       )
+BEGIN
+SELECT RAISE(IGNORE);
+END;
--- Parte3/triggers/gatilho3_verifica.sql
+++ Parte3/triggers/gatilho3_verifica.sql
@@ -1,1 +1,17 @@
 -- Só deixar um worker criar uma nova revisão se tiver feito da última revisão anteriormente
+
+.mode columns
+.headers on
+.nullvalue NULL
+
+SELECT * FROM CheckOut; --worker com id 11 fez checkout da ultima revisao do document 5, worker com id 10 não.
+
+SELECT * FROM Revision;
+
+INSERT INTO Revision (docID, authorID, version, date) VALUES (5, 10, 'Fixed some errors', '2020-5-05');
+
+SELECT * FROM Revision; --nao adiciona revisao
+
+INSERT INTO Revision (docID, authorID, version, date) VALUES (5, 11, 'Fixed some errors', '2020-5-05');
+
+SELECT * FROM Revision; --adiciona revisao
