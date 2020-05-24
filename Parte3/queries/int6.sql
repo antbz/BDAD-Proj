54c79efe328352a25238f71bcbab55363509e9d5
antbz <antonio.bezerra@fe.up.pt>
2020-05-24 17:11:42 +0100

    Improved int6

--- Parte3/queries/int6.sql
+++ Parte3/queries/int6.sql
@@ -6,11 +6,7 @@ FROM (
             Wname, 
             COUNT(docID) AS numdistDocs
     FROM (
-            SELECT DISTINCT Worker.WID, 
-                            Worker.Wname, 
-                            Document.docID, 
-                            Document.dNAME
-            FROM Worker, Document, DocumentRoleWorker
-            WHERE Worker.WID = DocumentRoleWorker.WID AND Document.docID = DocumentRoleWorker.docID
+            SELECT DISTINCT Worker.WID, Worker.Wname, docID
+            FROM DocumentRoleWorker JOIN Worker USING (WID)
         ) GROUP BY WID
 )WHERE numdistDocs=( SELECT COUNT(docID) FROM Document);
